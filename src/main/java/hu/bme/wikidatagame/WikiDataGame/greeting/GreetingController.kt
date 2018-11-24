41bb88fcf66ce3fbab61f199cea74e047ff7ae5a
Károlyi Patrik <karolyi.patrik@autsoft.hu>
2018-11-25 00:32:28 +0100

    genres in btns

--- src/main/java/hu/bme/wikidatagame/WikiDataGame/greeting/GreetingController.kt
+++ src/main/java/hu/bme/wikidatagame/WikiDataGame/greeting/GreetingController.kt
@@ -1,6 +1,10 @@
 package hu.bme.wikidatagame.WikiDataGame.greeting
 
+import hu.bme.wikidatagame.WikiDataGame.genre.GenreRepository
+import hu.bme.wikidatagame.WikiDataGame.movies.MovieRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.atomic.AtomicLong
 
@@ -16,6 +20,29 @@ class GreetingController {
 
     @PostMapping("/")
     fun greetingSubmit(@ModelAttribute greeting: Greeting): String {
+        return "index"
+    }
+
+    @Autowired
+    private
+    lateinit var movieRepository: MovieRepository
+
+    @Autowired
+    private
+    lateinit var genreRepository: GenreRepository
+
+
+    @GetMapping("/index")
+    fun getMovie(model : Model) : String {
+        val movie = movieRepository.findById(1).get()
+        val genres = genreRepository.findAll()
+        model.addAttribute("movie",movie)
+        model.addAttribute("genres",genres)
+        return "index"
+    }
+
+    @PostMapping("/index")
+    fun submitMovie(@ModelAttribute greeting: Greeting): String {
         return "index"
     }
 
